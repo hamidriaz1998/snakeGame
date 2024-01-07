1598666d8a332329b2a455a1ad74b1eaca11ae8b
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-07 14:25:45 +0500

    Initialize bonus class

--- bonus.py
+++ bonus.py
@@ -1,6 +1,16 @@
 from turtle import Turtle
+from random import randint as rand
+
+# Specify Screen Borders for random generation of bonus pill
+SCREENBORDERS = (-280, 280)
 
 
 class Bonus(Turtle):
     def __init__(self):
         super().__init__()
+        self.penup()
+        self.shapesize(0.5, 0.5)
+        self.shape("circle")
+        self.color("green")
+        self.speed("fastest")
+        self.goto(rand(*SCREENBORDERS), rand(*SCREENBORDERS))
