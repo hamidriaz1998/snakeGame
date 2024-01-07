21cc023669cd802ea1333d1e5d778fb7f638da4e
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-07 14:26:45 +0500

    Add bonus pill to the game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from time import sleep
 from snake import Snake
+from bonus import Bonus
 
 # Set Screen parameters
 screen = Screen()
@@ -9,8 +10,9 @@ screen.setup(height=600, width=600)
 screen.tracer(0)
 screen.title("Snake")
 
-# Initialize Snake
+# Initialize Snake and bonus pill
 snake = Snake()
+bonus = Bonus()
 
 # Listen for keystrokes
 screen.listen()
