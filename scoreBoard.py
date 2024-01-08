e20633aa71a2fa4659d03ebf3cfdb6d59dd504ca
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-08 12:10:36 +0500

    Refactor ScoreBoard class to improve readability and organization

--- scoreBoard.py
+++ scoreBoard.py
@@ -7,19 +7,20 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.penup()
+        self.color("white")
+        self.hideturtle()
         self.printScore()
+        self.goto(0, 280)
 
     def incrementScore(self):
         self.score += 1
 
     def printScore(self):
-        self.reset()
-        self.penup()
-        self.color("white")
-        self.goto(0, 280)
-        self.hideturtle()
+        self.clear()
         self.write(f"Score: {self.score}", align="Center", font=FONT)
 
     def gameOver(self):
+        self.clear()
         self.goto(0, 0)
         self.write("Game over", align="center", font=FONT)
